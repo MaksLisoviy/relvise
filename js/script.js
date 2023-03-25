bbf9b1103ecddfce7c7cafb079286af98ca6b46f
MaksLisoviy <128755236+MaksLisoviy@users.noreply.github.com>
2023-03-26 00:44:50 +0200

    Responsive financial services landing page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const iconMenu = document.querySelector('.menu__icon')
+if (iconMenu) {
+	const menuBody = document.querySelector('.menu__body')
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('lock');
+		iconMenu.classList.toggle('active');
+		menuBody.classList.toggle('active');
+	})
+}
